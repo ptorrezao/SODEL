266d020bd222e99a843151888a51b9233d626673
Pedro Torrezão <pedro.torrezao@gmail.com>
2019-01-19 23:13:48 +0000

    Final tweeks

---
+++ modules/digitalocean_droplep_operations/scripts/03-portainer.install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+COMPOSE_PATH="/tmp/scripts/docker-compose"
+ENV_FILE="/tmp/scripts/docker-compose/.env"
+for i in $(cat "$ENV_FILE") ; do export  $i; done
+
+COMPOSE_FILE_BASE="$COMPOSE_PATH/docker-compose.portainer.yml"
+COMPOSE_FILE_EXTEND="$COMPOSE_PATH/docker-compose.portainer.traefik.yml"
+
+docker stack deploy \
+  -c "$COMPOSE_FILE_BASE" \
+  -c "$COMPOSE_FILE_EXTEND" \
+  portainer
